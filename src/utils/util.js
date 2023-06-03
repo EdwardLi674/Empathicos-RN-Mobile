e55195649c69e25c59dfc8bdbb2103c098d28172
EdwardLi674 <edwardli674@gmail.com>
2023-06-03 07:06:32 -0700

    Paypal payment feature

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,1 +1,7 @@
 export const baseUrl = 'https://app.andreaplesha.com/api/store/v1';
+
+export const paypal_client_id =
+  'AaonRxizGmLe5JDndfx702aZZfLfzeGKtGiqmIy8ju3dk3wXoybLoi4yz28_eD1RCwHiOiIgbpTPbX9y';
+
+export const paypal_secret =
+  'EDSeLiX0OYg4p1wZ4alYFuqQJfuYxdCTx_OlmXws28Mo8cSpULO_1rl2p0Yu2L9N9BLZ04t4Zpz4-Cb1';
